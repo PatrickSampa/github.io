22d9e0bf6206d6068195160baceebcc09330af9a
PatrickSampa <sampa.mendonca@gmail.com>
2025-01-22 00:35:01 -0300

    refactor

--- my-perfil/src/components/HeaderComponent/Header.tsx
+++ my-perfil/src/components/HeaderComponent/Header.tsx
@@ -1,36 +1,28 @@
-import {
-  Wrapper,
-  HeaderContext,
-  MyAvatar,
-  MenuItens,
-  MenuList,
-  MenuItem,
-  Link,
-} from "./styled.header";
+import * as C from "./styled.header";
 import myAvatar from "../../assets/myAvatar.png";
 
 export const Header = () => {
   return (
-    <Wrapper>
-      <HeaderContext>
-        <MyAvatar src={myAvatar} alt="avatar" />
-        <MenuItens>
-          <MenuList>
-            <MenuItem>
-              <Link href="#home-section">{"<Home />"}</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="#about-section">{"<About />"}</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="#skills">{"<Skills />"}</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="#projects">{"<Projects />"}</Link>
-            </MenuItem>
-          </MenuList>
-        </MenuItens>
-      </HeaderContext>
-    </Wrapper>
+    <C.Wrapper>
+      <C.HeaderContext>
+        <C.MyAvatar src={myAvatar} alt="avatar" />
+        <C.MenuItens>
+          <C.MenuList>
+            <C.MenuItem>
+              <C.Link href="#home-section">{"<Home />"}</C.Link>
+            </C.MenuItem>
+            <C.MenuItem>
+              <C.Link href="#about-section">{"<About />"}</C.Link>
+            </C.MenuItem>
+            <C.MenuItem>
+              <C.Link href="#skills">{"<Skills />"}</C.Link>
+            </C.MenuItem>
+            <C.MenuItem>
+              <C.Link href="#projects">{"<Projects />"}</C.Link>
+            </C.MenuItem>
+          </C.MenuList>
+        </C.MenuItens>
+      </C.HeaderContext>
+    </C.Wrapper>
   );
 };
